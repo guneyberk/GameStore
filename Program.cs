a2e763a4bbba1289c818dcff27abb2595c510e01
BERK <guneybrk95@gmail.com>
2024-08-09 15:50:30 +0300

    COmment

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 const string GetGameEndpointName = "GetGame";   
-
+//not good for concurrency
 List<GameDto> games = [
 
     new (1,
@@ -23,7 +23,13 @@ List<GameDto> games = [
 ];
 app.MapGet("games",()=>games);
 
-app.MapGet("games/{id}",(int id)=>games.Find(game => game.Id == id)).WithName(GetGameEndpointName);//the way of doing rest 
+app.MapGet("games/{id}",(int id)=>
+{
+
+    GameDto? game=games.Find(game => game.Id == id);
+    return game is null ? Results.NotFound():Results.Ok(game);
+})
+    .WithName(GetGameEndpointName);//the way of doing rest 
 
 app.MapPost("games",(CreateGameDto newGame) =>{
     GameDto game = new(
@@ -36,6 +42,26 @@ app.MapPost("games",(CreateGameDto newGame) =>{
     games.Add(game);
 
     return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id},game);
+});
+
+app.MapPut("games/{id}", (int id, UpdateGameDto updateGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+    games[index] = new GameDto(
+        id,
+        updateGame.Name,
+        updateGame.Genre,
+        updateGame.Price, 
+        updateGame.ReleaseDate
+    );
+    return Results.NoContent();
+});
+
+app.MapDelete("games/{id}",(int id)=>
+{
+    games.RemoveAll(game => game.Id == id);
+    return Results.NoContent();
+    
 });
 
 app.Run();
